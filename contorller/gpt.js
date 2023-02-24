02428ad232dc8a2d1572aef1d28d677aeb5a8980
lsy <lesiyu129@163.com>
2023-02-24 17:09:12 +0800

    接入 gpt

--- contorller/gpt.js
+++ contorller/gpt.js
@@ -1,11 +1,11 @@
 const { Configuration, OpenAIApi } = require("openai");
 const { code } = require("../config/code");
 const MyError = require("../unit/myError");
-const crypto = require("crypto")
+const crypto = require("crypto");
 const sha1 = require("sha1");
 
 class gpt {
-    constructor(){
+    constructor() {
         this.sendMsg = (xml, msg) => {
             const { ToUserName, FromUserName } = xml;
             return `
@@ -17,7 +17,33 @@ class gpt {
             <Content><![CDATA[${msg}]]></Content>
           </xml>
          `;
-        }
+        };
+        this.message = async (prompt, apiKey) => {
+            const promptStr = `\n${prompt}`;
+            if (!apiKey) {
+                throw new MyError({ code: code.notKey, message: 'apiKey不能为空' });
+            }
+            const configuration = new Configuration({
+                apiKey: apiKey || "apiKey"
+            });
+            const openai = new OpenAIApi(configuration);
+            try {
+                const response = await openai.createCompletion({
+                    best_of: 1,
+                    model: "text-davinci-003",
+                    prompt: promptStr,
+                    temperature: 0.5,
+                    max_tokens: 2038,
+                    top_p: 1,
+                    frequency_penalty: 0.0,
+                    presence_penalty: 0.0,
+                    // stop: "\n"
+                });
+                return response.data;
+            } catch (error) {
+                return error;
+            }
+        };
     }
     async request(ctx) {
         const body = ctx.request.body;
@@ -49,32 +75,31 @@ class gpt {
         }
 
     }
-    async wechat(ctx){
+    async wechat(ctx) {
         const query = ctx.query;
-        const {signature,timestamp,nonce,echostr} = query
-        const token = 'incar2023chatgpt'
-        const soltStr = [nonce,timestamp,token].sort().join('')
-        const sha1Str = sha1(soltStr)
+        const { signature, timestamp, nonce, echostr } = query;
+        const token = 'incar2023chatgpt';
+        const soltStr = [nonce, timestamp, token].sort().join('');
+        const sha1Str = sha1(soltStr);
         if (sha1Str === signature) {
-            ctx.body = echostr
-          } else {
-            console.log('error')
+            ctx.body = echostr;
+        } else {
+            console.log('error');
         }
     }
-    async sendMessage(ctx){
-        const { xml } = ctx.req.body 
-        const { ToUserName, FromUserName, CreateTime, MsgType, Content, MsgId } = xml
+    async sendMessage(ctx) {
+        const { xml } = ctx.req.body;
+        const { ToUserName, FromUserName, CreateTime, MsgType, Content, MsgId } = xml;
         if (MsgType == 'text') {
-            if (Content == 'hello') {
-                const responseMSg = 'world';
-                // ctx.res.setHeader('Content-Type', 'text/xml')
-                ctx.body=this.sendMsg(xml, responseMSg)
-            }
-        }else {
-            ctx.body = '机器人正在检修中'
-        } 
-
-        console.log(ctx);
+            const responseMSg = await this.message(Content[0], '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');
+            let str = ''
+            responseMSg.choices.forEach(element => {
+                str += element.text;
+            })
+            ctx.body = this.sendMsg(xml, str);
+        } else {
+            ctx.body = this.sendMsg(xml, '机器人正在检修中');
+        }
     }
 }
 
